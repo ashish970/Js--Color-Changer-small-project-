5ab11880fd2b6b1aa5c2b0c8917f32fefcd7a7f1
Ashish <ashish9gps@gmail.com>
2023-09-25 22:01:42 +0530

    Add files via upload
    this is final commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("button");
+const randomColour = ()=> {
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+
+    for(let i = 0; i<6; i++)
+    {
+        cons = cons + val[Math.floor(Math.random() * 16)];
+    }
+
+    return cons;
+};
+
+console.log(randomColour);
+
+function changeRandomColour(){
+ document.body.style.backgroundColor = randomColour();
+
+}
+
+btn.addEventListener("click", changeRandomColour);
